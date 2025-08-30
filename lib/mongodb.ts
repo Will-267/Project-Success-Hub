2459fab1b6aa94005a84200c7ab091bd4ce80f16
Will-267 <Williameleazar51@gmail.com>
2025-08-30 16:04:32 +0100

    fix: update MongoDB URI and upgrade Next.js version in package.json and package-lock.json

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb+srv://williameleazar51:6koGY52UtqMHXYVw@cluster0.1nkjt.mongodb.net/"
 
 if (!MONGODB_URI) {
   throw new Error(
